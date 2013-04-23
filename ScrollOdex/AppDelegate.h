85283167a94a8a81caffba60588d30e0d2481c72
Manasi Shah <oabstudios@oabs-Mac-mini.local>
2013-04-23 10:49:25 +0530

    Initial Commit

---
+++ ScrollOdex/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ScrollOdex
+//
+//  Created by Manasi Shah on 23/04/13.
+//  Copyright (c) 2013 OAB Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ScrollOdex/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ScrollOdex
+//
+//  Created by Manasi Shah on 23/04/13.
+//  Copyright (c) 2013 OAB Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
